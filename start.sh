9fd2254d75bf5d13c1b1ad08fc973adaf93c0389
SiDK <127277028+SiDK-FM@users.noreply.github.com>
2023-06-03 17:23:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/SiDK-FM/Rrrrr /Rrrrr
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO Rrrrr
+  git clone $UPSTREAM_REPO /Rrrrr
 fi
-cd Rrrrr
+cd /Rrrrr
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
